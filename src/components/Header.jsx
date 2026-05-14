3c91ec56703c99796f41a29fb80301e600a6aa0a
syafitri24si-framework <syafitri24si@mahasiswa.pcr.ac.id>
2026-05-14 22:14:03 +0700

    ubah" aja

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,6 +56,7 @@ export default function Header() {
     />
             <div className="absolute -bottom-2 -right-1 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
           </div>
+          
         </div>
 
       </div>
